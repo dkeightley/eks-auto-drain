cdc6fa90c9852e5a005c21efd4d63ffed3455644
dkeightley <derekjkeightley@gmail.com>
2019-05-31 16:28:26 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,133 @@
+# EKS Auto Drain
+
+Gracefully drain EKS Worker Nodes whenever a node is terminated by an Auto Scaling Group or a Spot termination.
+
+Deployable Lambda function with CloudWatch Event Rules and an IAM Role, enabled by adding a Lifecycle hook to any Auto Scaling Group in the same AWS Region.
+
+## Deploy
+
+Deployment of the Lambda, IAM Role and Cloudwatch Event Rules can be simplified with the SAM CLI and Docker.
+
+[SAM CLI can be installed](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) differently depending on your OS, in general for Linux and MacOS however..
+
+```bash
+brew upgrade
+brew update
+brew tap aws/tap
+brew install aws-sam-cli
+sam --version
+```
+
+### Build, Package, and Deploy using SAM
+
+* Clone this repository
+
+```bash
+git clone https://github.com/dkeightley/eks-auto-drain.git
+cd eks-auto-drain
+```
+
+* Optional: set your AWS region and create an S3 bucket
+
+```bash
+export AWS_DEFAULT_REGION=<region name>
+aws s3 mb s3://<bucket name>
+```
+
+* Build, package and deploy the project with SAM
+
+```bash
+sam build --use-container
+sam package --output-template-file packaged.yaml --s3-bucket <bucket name>
+sam deploy --template-file packaged.yaml --stack-name eks-auto-drain --capabilities CAPABILITY_IAM
+```
+
+## Configure
+
+To provide RBAC permissions for the drain, an RBAC group that provides the specific permissions is needed. Once added the Lambda execution role can be mapped to the group in the `aws-auth` ConfigMap for each EKS Cluster
+
+* Deploy the RBAC ClusterRole and ClusterRoleBinding for each Cluster
+
+```bash
+kubectl apply -f rbac/
+```
+
+* Obtain the Lambda execution Role
+
+```bash
+aws cloudformation describe-stacks --stack-name eks-auto-drain --query 'Stacks[0].Outputs[0].OutputValue'
+ ```
+
+* Add a mapping for the Role to the ClusterRole for each Cluster
+\
+Use an imperative action, like edit, to add to the ConfigMap to avoid merge conflicts
+
+```bash
+kubectl edit -n kube-system configmap aws-auth
+```
+
+Example:
+
+```yaml
+mapRoles: |
+    - groups:
+      - eks-auto-drain-lambda
+      rolearn: <Lambda execution Role>
+      username: eks-auto-drain-lambda
+```
+
+* Add a Lifecycle hook to each Auto Scaling Group for the Nodes in each Cluster
+
+Define a variable to loop through these, otherwise the below command can be used for each ASG
+
+`aws autoscaling put-lifecycle-hook --lifecycle-hook-name eks-auto-drain --lifecycle-transition "autoscaling:EC2_INSTANCE_TERMINATING" --heartbeat-timeout 300 --default-result CONTINUE --auto-scaling-group-name <auto scaling group name>`
+
+```bash
+ASGS="group-1 group-2 group-3"
+```
+
+```bash
+for i in $ASGS
+  do
+    aws autoscaling put-lifecycle-hook --lifecycle-hook-name eks-auto-drain --lifecycle-transition "autoscaling:EC2_INSTANCE_TERMINATING" --heartbeat-timeout 300 --default-result CONTINUE --auto-scaling-group-name $i
+done
+```
+
+# Test
+
+### Local testing with the SAM CLI
+
+```bash
+sam local invoke -e misc/event.json
+```
+
+### Testing by terminating an instance
+
+Obtain a list of instances in a Cluster:
+
+`kubectl get nodes -o=custom-columns=NAME:.metadata.name,INSTANCE:.spec.providerID `
+
+Test by terminating an instance in an ASG
+
+```bash
+aws autoscaling terminate-instance-in-auto-scaling-group --no-should-decrement-desired-capacity --instance-id <instance id>
+```
+
+## Cleanup
+
+```bash
+kubectl delete -f rbac/
+aws cloudformation delete-stack --stack-name eks-auto-drain
+```
+
+```bash
+ASGS="group-1 group-2 group-3
+for i in $ASGS
+  do
+    aws autoscaling delete-lifecycle-hook --lifecycle-hook-name eks-auto-drain  --auto-scaling-group-name $i
+done
+```
+
+## TODO
+
+* VPC support for private access
---
+++ src/lambda_function.py
@@ -0,0 +1,258 @@
+import base64
+import boto3
+import re
+import yaml
+import logging
+import os
+import sys
+import time
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+from botocore.signers import RequestSigner
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Dry run mode if supported [boolean]
+dry_run = False
+# Optional delay before signalling completed to the ASG lifecycle [seconds]
+# The pod delete API is async so a delay allows pods to graceful stop/drain before node termination proceeds
+# This should ideally cover the maximum terminationGracePeriodSeconds (default 30s)
+delay = 30
+
+region = os.environ['AWS_DEFAULT_REGION']
+cluster_state = '/tmp/cluster_name'
+kubeconfig = '/tmp/kubeconfig'
+
+def process_lifecycle(detail):
+
+    same_cluster = False
+    cluster_name = None
+    instance = detail["EC2InstanceId"]
+    ec2 = boto3.client('ec2', region_name=region)
+
+    # Describe the instance so we can define the node_name and cluster_name from the response
+    logger.info('Received event for {} in {}'.format(instance, region))
+    try:
+        instance_describe = ec2.describe_instances(InstanceIds=[instance])
+    except ClientError as err:
+        logger.exception('Problem describing instance {}'.format(instance))
+        logger.exception(err.response)
+        sys.exit(1)
+
+    # Define node_name and cluster_name
+    node_name = instance_describe["Reservations"][0]["Instances"][0]["PrivateDnsName"]
+    for tags in instance_describe["Reservations"][0]["Instances"][0]["Tags"]:
+
+        if tags["Key"] == 'KubernetesCluster':
+            cluster_name = tags["Value"]
+        
+    if cluster_name is None:
+        logger.exception('This instance doesn\'t appear to have a KubernetesCluster tag, exiting...')
+        sys.exit(0)
+    
+    logger.info('Processing event for {} in the {} cluster'.format(node_name, cluster_name))
+
+    # If the same cluster_name is configured ignore creating the kubeconfig
+    if os.path.exists(cluster_state):
+        with open(cluster_state) as state:
+            contents = state.read()
+            search_word = cluster_name
+
+            if search_word in contents:
+                logger.info('We are working with the same cluster, skipping the kubeconfig build')
+                same_cluster = True
+   
+    if same_cluster is not True:
+
+        if os.path.exists(cluster_state):
+            os.remove(cluster_state)
+       
+        capture_state = open(cluster_state, "w")
+        capture_state.write(cluster_name)
+        capture_state.close()
+
+        if os.path.exists(kubeconfig):
+            os.remove(kubeconfig)
+
+        logger.info('Building the kubeconfig file for {}'.format(cluster_name))
+        create_kubeconfig(cluster_name)
+
+    logger.info('Generating bearer token')
+    token = get_bearer_token(cluster_name)
+
+    # Configure the kubenetes client
+    config.load_kube_config(kubeconfig)
+    configuration = client.Configuration()
+    configuration.api_key['authorization'] = token
+    configuration.api_key_prefix['authorization'] = 'Bearer'
+
+    # Configure the API 
+    api = client.ApiClient(configuration)
+    v1 = client.CoreV1Api(api)
+
+    try:
+        if not node_exists(v1, node_name):
+            # Don't delay if the node already doesn't exist
+            global delay
+            del delay
+            complete_lifecycle(detail) 
+            return
+
+        # Cordon the node, sleep for 2s remove all pods and complete the lifecycle
+        cordon_node(v1, node_name)
+        time.sleep(2)
+        remove_all_pods(v1, node_name)
+        complete_lifecycle(detail)
+
+    except ApiException:
+        logger.exception('There was an error removing the Pods from the Node {}'.format(node_name))
+        complete_lifecycle(detail) 
+
+def create_kubeconfig(cluster_name):
+        
+    kube_content = dict()
+    # Get data from EKS API
+    eks_api = boto3.client('eks')
+    cluster_info = eks_api.describe_cluster(name=cluster_name)
+    certificate = cluster_info['cluster']['certificateAuthority']['data']
+    endpoint = cluster_info['cluster']['endpoint']
+
+    # Generating kubeconfig
+    kube_content = dict()
+    
+    kube_content['apiVersion'] = 'v1'
+    kube_content['clusters'] = [
+        {
+        'cluster':
+            {
+            'server': endpoint,
+            'certificate-authority-data': certificate
+            },
+        'name':'kubernetes'
+                
+        }]
+
+    kube_content['contexts'] = [
+        {
+        'context':
+            {
+            'cluster':'kubernetes',
+            'user':'aws'
+            },
+        'name':'aws'
+        }]
+
+    kube_content['current-context'] = 'aws'
+    kube_content['Kind'] = 'config'
+    kube_content['users'] = [
+    {
+    'name':'aws',
+    'user':'lambda'
+    }]
+
+    # Write the kubeconfig
+    with open(kubeconfig, 'w') as outfile:
+        yaml.dump(kube_content, outfile, default_flow_style=False)
+
+def get_bearer_token(cluster_name):
+
+    STS_TOKEN_EXPIRES_IN = 60
+    session = boto3.session.Session()
+
+    client = session.client('sts')
+    service_id = client.meta.service_model.service_id
+
+    signer = RequestSigner(
+        service_id,
+        region,
+        'sts',
+        'v4',
+        session.get_credentials(),
+        session.events
+    )
+
+    params = {
+        'method': 'GET',
+        'url': 'https://sts.{}.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15'.format(region),
+        'body': {},
+        'headers': {
+            'x-k8s-aws-id': cluster_name
+        },
+        'context': {}
+    }
+
+    signed_url = signer.generate_presigned_url(
+        params,
+        region_name=region,
+        expires_in=STS_TOKEN_EXPIRES_IN,
+        operation_name=''
+    )
+
+    base64_url = base64.urlsafe_b64encode(signed_url.encode('utf-8')).decode('utf-8')
+
+    # remove any base64 encoding padding:
+    return 'k8s-aws-v1.' + re.sub(r'=*', '', base64_url)
+
+def cordon_node(api, node_name):
+    # Marks the specified node as unschedulable, which means that no new pods can be launched on the 
+    # node by the Kubernetes scheduler
+    patch_body = {
+        'apiVersion': 'v1',
+        'kind': 'Node',
+        'metadata': {
+            'name': node_name
+        },
+        'spec': {
+            'unschedulable': True
+        }
+    }
+
+    api.patch_node(node_name, patch_body)
+
+def remove_all_pods(api, node_name):
+    # Removes all Kubernetes pods from the specified node
+    field_selector = 'spec.nodeName=' + node_name
+    pods = api.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
+
+    logger.debug('Number of pods to delete: ' + str(len(pods.items)))
+
+    for pod in pods.items:
+        logger.info('Deleting pod {} in namespace {}'.format(pod.metadata.name, pod.metadata.namespace))
+        body = {
+            'apiVersion': 'policy/v1beta1',
+            'kind': 'Eviction',
+            'metadata': {
+                'name': pod.metadata.name,
+                'namespace': pod.metadata.namespace
+            }
+        }
+        api.create_namespaced_pod_eviction(pod.metadata.name + '-eviction', pod.metadata.namespace, body)
+
+def node_exists(api, node_name):
+    # Determines whether the specified node is still part of the cluster
+    nodes = api.list_node(include_uninitialized=True, pretty=True).items
+    node = next((n for n in nodes if n.metadata.name == node_name), None)
+    return False if not node else True
+
+def complete_lifecycle(detail):
+
+    if 'delay' in globals():
+        # Sleep if delay is defined, then complete the ASG lifecycle (terminate the node)
+        time.sleep(delay)
+
+    logger.info('Completing the ASG lifecycle action')
+    asg = boto3.client('autoscaling', region_name=region)
+    asg.complete_lifecycle_action(
+        LifecycleHookName=detail["LifecycleHookName"],
+        AutoScalingGroupName=detail["AutoScalingGroupName"],
+        LifecycleActionToken=detail["LifecycleActionToken"],
+        LifecycleActionResult="ABANDON",
+        InstanceId=detail["EC2InstanceId"]
+    )
+
+def lambda_handler(event, context):
+    
+    detail = event["detail"]
+    process_lifecycle(detail)
